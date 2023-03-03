eba289ed8b3323e112ac7147e1f71cad8f38a9bc
Ibrahim Fathi <117414627+Ibrahimfathi96@users.noreply.github.com>
2023-03-03 03:36:05 +0200

    need edit seb7a header to rotate with every click

--- lib/home/sebha_tab/sebha_tab.dart
+++ lib/home/sebha_tab/sebha_tab.dart
@@ -26,6 +26,7 @@ class _SebhaScreenContentState extends State<SebhaScreenContent> {
               });
             },
             child: Image.asset(
+              /// TODO Make this image body rotatable
               settingsProvider.currentTheme == ThemeMode.dark
                   ? 'assets/images/sebha_dark.png'
                   : 'assets/images/sebha_header.png',
